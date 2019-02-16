472e18af3f50ee42333a757363f30f9fc8bd73b5
Sam Huang <sam.t.huang@me.com>
2019-02-16 10:56:43 +0900

    added api post call to get the text difficulty

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="getDifficulty">
+      get text difficulty
+    </button>
   </div>
 </template>
 
@@ -11,8 +13,22 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      api: "https://twinword-language-scoring.p.rapidapi.com/text/",
+      text: "Losses were down 15 percent for the full year over 2017, but still reached an adjusted $1.8 billion. That could pose a challenge to investors trying to figure out Uber's value in the public markets. Last year, bankers vying to lead the company's initial public offering told Uber the market could value it at $120 billion. News that the company is still burning through more than $1 billion annually may give some investors pause."
+    }
+  },
+  methods: {
+    getDifficulty() {
+      const unirest = require('unirest');
+      unirest.post(this.api)
+             .header("X-RapidAPI-Key", "d1a329222bmsh48b7736d3c61852p1b73b0jsn1c0077714657")
+             .send({text: this.text})
+             .end(function (result) {
+               console.log(`the difficulty level is: ${result.body.ten_degree}`)
+             })
+    }
   }
 }
 </script>
