1663c7af785e0a88c391361e226041f91c592edc
Hiroki Furuichi <aivvinna@gmail.com>
2019-02-17 12:50:59 +0900

    cleaned up code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <h1>News Reader</h1>
-    </div> -->
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,5 @@
 <template>
   <div class="home">
-    <!-- <div class="columns"> -->
-      <!-- <div class="column"> -->
-        <!-- <button
-          v-show="index > 0"
-          @click="prevNews"
-          class="is-large">
-          <span class="icon">
-            <i class="fas fa-arrow-left"></i>
-          </span>
-        </button> -->
-      <!-- </div> -->
-      <!-- <div class="column"> -->
-        <!-- <button
-          v-show="$store.state.article.content"
-          @click="readText(article.content)" >
-          Play
-        </button> -->
-      <!-- </div>
-      <div class="column"> -->
-        <!-- <button
-          @click="nextNews"
-          class="is-large">
-          <span class="icon">
-            <i class="fas fa-arrow-right"></i>
-          </span>
-        </button> -->
-      <!-- </div> -->
-    <!-- </div> -->
     <button class="round-button"
       @mousedown="recordCommand();addMicrophone();"
       @touchstart="recordCommand();addMicrophone();"
@@ -39,23 +11,15 @@
       <div class="pulse-ring">
       </div>
     </button>
-    <!-- <ArticleCard class="column" /> -->
   </div>
 </template>
 
 <script>
-import { VoiceRSS } from "../util/voicerss-tts.min.js"
 import { mapGetters } from 'vuex'
-
-import ArticleCard from '../components/ArticleCard.vue'
-
 import axios from 'axios'
 
 export default {
   name: 'home',
-  components: {
-    ArticleCard
-  },
   data() {
     return {
       recognition: "",
@@ -66,7 +30,6 @@ export default {
     }
   },
   mounted() {
-    console.log('mounted, querying db for news');
     window.addEventListener('keydown', e => {
       if (e.keyCode === 37) {
         this.prevNews();
@@ -131,18 +94,6 @@ export default {
     }
   },
   methods: {
-    readText(text) {
-      console.log(text)
-      VoiceRSS.speech({
-        key: '7f63364aebe443a9abc5a8a6b6e3ef5b',
-        src: text,
-        hl: 'en-us',
-        r: 0, 
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
-      });
-    },
     betterReadText(text) {
       this.speech = new SpeechSynthesisUtterance(text);
       this.speech.lang = 'en-US';
@@ -153,7 +104,6 @@ export default {
       this.recognition = new window.SpeechRecognition();
       this.recognition.continuous = true;
       this.recognition.onresult = (event) => {
-        console.log('transscript: ', event.results[event.results.length -1][0].transcript);
         this.word = event.results[event.results.length -1][0].transcript;
       }
       this.recognition.start()
@@ -171,9 +121,7 @@ export default {
       this.word = ""
     },
     async getNews() {
-      console.log('getting news')
       await this.$store.dispatch('getArticles')
-      console.log(this.$store.state.articles)
     },
     prevNews() {
       if (this.index !== 0) {
@@ -183,7 +131,6 @@ export default {
     },
     nextNews() {
       this.$store.dispatch('setArticle', this.articles[this.index + 1])
-      console.log(this.article)
     }
   }
 }
@@ -227,7 +174,6 @@ export default {
     }
   .fa-microphone {
     animation: pulsate infinite 3s;
-
     @-webkit-keyframes pulsate {
       0% {
         -webkit-transform: scale(1, 1);
